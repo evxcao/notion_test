90b765f780b04fbfccdf51581b0381d2e927bc2a
Carlos Arturo Osorio Sandoval <31473483+evxcao@users.noreply.github.com>
2024-02-15 10:35:52 +0000

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -11,6 +11,6 @@ function calculate(){
   return{
     DL_W1_1: 2.5 * Math.round((deadliftWeight*0.65)/2.5),
     DL_W1_2: 2.5 * Math.round((deadliftWeight*0.75)/2.5),
-    DL_W1_3: 2.5 * Math.round((deadliftWeight*0.85)/2.5),
+    DL_W1_3: 2.5 * Math.round((deadliftWeight*0.85)/2.5)};
 };
 
