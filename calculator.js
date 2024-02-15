2ae1aa3aff42ca640a58afaac9b6dc173b994bd5
Carlos Arturo Osorio Sandoval <31473483+evxcao@users.noreply.github.com>
2024-02-15 11:52:25 +0000

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,14 +1,13 @@
 var deadliftWeight = 125;
 
 
-function calculate(){
+window.onload = function calculate(){
     var wpDL1 = 2.5 * Math.round((deadliftWeight*0.40)/2.5);
     document.getElementById('DL_WP1').innerHTML = wpDL1;
   //  DL_W1_1: 2.5 * Math.round((deadliftWeight*0.65)/2.5),
   //  DL_W1_2: 2.5 * Math.round((deadliftWeight*0.75)/2.5),
   //  DL_W1_3: 2.5 * Math.round((deadliftWeight*0.85)/2.5),
-    alert("hola");
-}
+};
 
 
 
