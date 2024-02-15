e4511d6fa81fc0415038de53127302724cf14a8d
Carlos Arturo Osorio Sandoval <31473483+evxcao@users.noreply.github.com>
2024-02-15 12:51:48 +0000

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
+var squatWeight = 117.5;
 var deadliftWeight = 125;
 
-window.onload = function calculateDL(){
+window.onload = function calculate(){
     var wpDL1 = 2.5 * Math.round((deadliftWeight*0.40)/2.5);
     document.getElementById('DL_WP1').innerHTML = wpDL1;
     var wpDL2 = 2.5 * Math.round((deadliftWeight*0.50)/2.5);
@@ -13,5 +14,12 @@ window.onload = function calculateDL(){
     document.getElementById('DL_W12').innerHTML = w1DL2;
     var w1DL3 = 2.5 * Math.round((deadliftWeight*0.85)/2.5);
     document.getElementById('DL_W13').innerHTML = w1DL3;
+
+    var wpSQ1 = 2.5 * Math.round((deadliftWeight*0.40)/2.5);
+    document.getElementById('SQ_WP1').innerHTML = wpDL1;
+    //var wpDL2 = 2.5 * Math.round((deadliftWeight*0.50)/2.5);
+    //document.getElementById('SQ_WP2').innerHTML = wpDQ2;
+    var wpSQ3 = 2.5 * Math.round((deadliftWeight*0.60)/2.5);
+    document.getElementById('SQ_WP3').innerHTML = wpSQ3;
 };
 //random
