eb0f15cf8deca70a1177bb509c7b726a660725e1
Carlos Arturo Osorio Sandoval <31473483+evxcao@users.noreply.github.com>
2024-02-15 13:15:47 +0000

    Update DL_A.js

--- js/DL_A.js
+++ js/DL_A.js
@@ -1,1 +1,19 @@
+window.onload = function calculate(){
+    var wpDL1 = 2.5 * Math.round((deadliftWeight*0.40)/2.5);
+    document.getElementById('DL_WP1').innerHTML = wpDL1;
+    var wpDL2 = 2.5 * Math.round((deadliftWeight*0.50)/2.5);
+    document.getElementById('DL_WP2').innerHTML = wpDL2;
+    var wpDL3 = 2.5 * Math.round((deadliftWeight*0.60)/2.5);
+    document.getElementById('DL_WP3').innerHTML = wpDL3;
+    var w1DL1 = 2.5 * Math.round((deadliftWeight*0.65)/2.5);
+    document.getElementById('DL_W11').innerHTML = w1DL1;
+    var w1DL2 = 2.5 * Math.round((deadliftWeight*0.75)/2.5);
+    document.getElementById('DL_W12').innerHTML = w1DL2;
+    var w1DL3 = 2.5 * Math.round((deadliftWeight*0.85)/2.5);
+    document.getElementById('DL_W13').innerHTML = w1DL3;
 
+    var wpSQ1 = 2.5 * Math.round((squatWeight*0.40)/2.5);
+    document.getElementById('SQ_WP1').innerHTML = wpSQ1;
+    var wpSQ3 = 2.5 * Math.round((squatWeight*0.60)/2.5);
+    document.getElementById('SQ_WP3').innerHTML = wpSQ3;
+};
