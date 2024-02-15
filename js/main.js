5f0a030bfb42065d493ab885f32d6f3d33df3963
Carlos Arturo Osorio Sandoval <31473483+evxcao@users.noreply.github.com>
2024-02-15 16:50:31 +0000

    Update and rename DL.js to main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-window.onload = function calculateDL(){
+window.onload = function calculate(){
+    var SQ40 = 2.5 * Math.round((squatWeight*0.40)/2.5);
+    var SQ50 = 2.5 * Math.round((squatWeight*0.50)/2.5);
+    var SQ60 = 2.5 * Math.round((squatWeight*0.60)/2.5);
+    var SQ65 = 2.5 * Math.round((squatWeight*0.65)/2.5);
+    var SQ70 = 2.5 * Math.round((squatWeight*0.70)/2.5);
+    var SQ75 = 2.5 * Math.round((squatWeight*0.75)/2.5);
+    var SQ80 = 2.5 * Math.round((squatWeight*0.80)/2.5);
+    var SQ85 = 2.5 * Math.round((squatWeight*0.85)/2.5);
+    var SQ90 = 2.5 * Math.round((squatWeight*0.90)/2.5);
+    var SQ95 = 2.5 * Math.round((squatWeight*0.95)/2.5);
+    
     var DL40 = 2.5 * Math.round((deadliftWeight*0.40)/2.5);
     var DL50 = 2.5 * Math.round((deadliftWeight*0.50)/2.5);
     var DL60 = 2.5 * Math.round((deadliftWeight*0.60)/2.5);
@@ -17,6 +28,17 @@ window.onload = function calculateDL(){
         }
         return element;
     }
+    
+    setText("SQ40",SQ40);
+    setText("SQ50",SQ50);
+    setText("SQ60",SQ60);
+    setText("SQ65",SQ65);
+    setText("SQ70",SQ70);
+    setText("SQ75",SQ75);
+    setText("SQ80",SQ80);
+    setText("SQ85",SQ85);
+    setText("SQ90",SQ90);
+    setText("SQ95",SQ95);    
 
     setText("DL40",DL40);
     setText("DL50",DL50);    
