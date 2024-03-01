52b68f4773b545d4828bed08df5f7c2067f423e8
Carlos Arturo Osorio Sandoval <31473483+evxcao@users.noreply.github.com>
2024-03-01 11:32:15 +0000

    Update trainingMax.js

--- js/trainingMax.js
+++ js/trainingMax.js
@@ -1,4 +1,7 @@
-var squatWeight = 117.5;
-var benchWeight = 62.5
-var deadliftWeight = 125;
-var pressWeight = 45;
+// If C 1+ set >= 3, increase by 5
+var squatWeight = 105; // down
+var deadliftWeight = 130; // up
+
+// If C 1+ set >= 3, increase by 2.5
+var benchWeight = 65; // up
+var pressWeight = 47.5; // up
